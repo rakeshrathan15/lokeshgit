789cc618db37ade2f3f009d81204a2bbdc75a2f6
rakesh mekala <rakeshyadavmekala15@gmail.com>
2025-01-17 10:38:43 +0530

    i have done changes in Hello Wolrd code

---
+++ src/main/java/com/neoteric/App.java
@@ -0,0 +1,14 @@
+package com.neoteric;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
